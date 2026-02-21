b734ceb39a7b6103629250be8b0fd57376d5facc
max-matinpalo <matinpalo.m@gmail.com>
2026-02-21 12:52:32 +0200

    12:52

--- template/src/_setup/main.jsx
+++ template/src/_setup/main.jsx
@@ -7,16 +7,22 @@ import App from '../App.jsx';
 
 // Mainstream js developers think globals are horrible
 // But because we are C developers - We love them 😅
+
 import { useState, useEffect, useRef } from "react";
-Object.assign(globalThis, { useState, useEffect, useRef });
+globalThis.useState = useState;
+globalThis.useEffect = useEffect;
+globalThis.useRef = useRef;
 
 globalThis.ENV = import.meta.env;
+globalThis.JOIN_CLASSES = (...classes) => classes.filter(Boolean).join(" ");
+
 
 if ("serviceWorker" in navigator) {
 	window.addEventListener("load", () => {
 		navigator.serviceWorker.register("/serviceWorker.js");
 	});
 }
+
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
